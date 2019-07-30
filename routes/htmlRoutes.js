01d7af1fb25d94a45677eca2c7cefa11439ce8b4
Keiko M <vikakeiko21@gmail.com>
2019-07-30 12:36:53 -0700

    initial

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,10 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
+
+
+
+
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
@@ -12,11 +16,26 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/community", function(req, res) {
+    // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      console.log('community')
+      res.render("community", {
+        example: ''
+      // });
+    });
+  });
+
+  app.get("/workshop", function(req, res) {
+    console.log("community")
+    res.render("workshop", {
+      example: ''
+    });
+  });
+
+  app.get("/library", function(req, res) {
+    console.log("library")
+    res.render("library", {
+      example: ''
     });
   });
 
