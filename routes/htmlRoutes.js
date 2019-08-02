274e5979873b86b03830282debc168571bc982d9
Keiko M <vikakeiko21@gmail.com>
2019-08-02 09:44:40 -0700

    done with api library category, connected json object to front end, add button to handlebar, created js file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,13 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
 
-  // GOOD =)
+
   app.get("/", function(req, res){
     res.render("index");
   })
 
   // Load example page and pass in an example by id
   app.get("/community", function(req, res) {
-    // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
     db.User.findAll({}).then(function (data){
       console.log('community')
       res.render("community", {data});
@@ -25,7 +24,6 @@ module.exports = function(app) {
       console.log(data);
       res.render("library", {data});
     })
-     
   });
 
   // Render 404 page for any unmatched routes
