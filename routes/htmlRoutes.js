b2db437b03d9a249e4bf32094faf8b81afeb2623
Donirvin Dickerson <Dond2018@gmail.com>
2019-08-02 09:18:23 -0700

    Update community handlebar

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,34 +1,37 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
 
   // GOOD =)
-  app.get("/", function(req, res){
+  app.get("/", function (req, res) {
     res.render("index");
   })
 
   // Load example page and pass in an example by id
-  app.get("/community", function(req, res) {
+  app.get("/community", function (req, res) {
     // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.User.findAll({}).then(function (data) {
       console.log('community')
-      res.render("community", {libraries: data 
-    });
+      res.render("community", {
+        libraries: data
+      });
+    })
   });
 
-  app.get("/library", function(req, res) {
+  app.get("/library", function (req, res) {
     console.log("library");
     db.Library.findAll({})
-    .then(function(data){
-      // console.log("==========   ", data);
-      console.log(data);
-      res.render("library", {data});
-    })
-     
+      .then(function (data) {
+        // console.log("==========   ", data);
+        console.log(data);
+        res.render("library", { data });
+      })
+
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
